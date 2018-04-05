b30c11799acb58a43f90d69240e3e070afccbc01
katcozadd <krosecozadd@gmail.com>
2018-04-05 12:16:17 -0600

    Linked all the files to each other for initial pages

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	console.log('sanity check');
+//Calling the canvas ID and creating a variable name for it
+let canvas = $('#gameCanvas');
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(canvas);
+
+
+
+})
