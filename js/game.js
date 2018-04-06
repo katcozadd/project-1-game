8cf3535dc0ae13156e3e4ee32642433b42cbd82f
katcozadd <krosecozadd@gmail.com>
2018-04-06 13:03:14 -0600

    Changed from canvas to jquery and lost my mind a little bit

--- js/game.js
+++ js/game.js
@@ -1,30 +1,78 @@
-let canvas = document.getElementById('gameCanvas');
-let context = canvas.getContext('2d');
-let imgDrops;
-let xPosition = 0;
-let yPosition = 0;
-let numberOfVegs = 50;
-let numberOfMeats = 25;
-var fallingItems = [];
+$(document).ready(function() {
+	console.log('sanity check');
+	
 
 
+	//creating a start point for vickie to start moving
+	let vickieStart = 0;
+
+	$(document).keydown(function(event) {
+	//Moving with right arrow key
 
 
-function move(event) {
-	if (event.keyCode === 39) {
-		xPosition += 20;
+        if(event.keyCode == "37")
+            $("#vickie").stop(true).animate({"left" : "-=50px"});
+
+        if(event.keyCode == "39")
+            $("#vickie").stop(true).animate({"left" : "+=50px"});	
+
 	}
-	if (event.keyCode === 37) {
-		xPosition -= 20;
-	}
-
-	canvas.width = canvas.width;
-	context.rect(xPosition, yPosition, 20, 50);
-	context.stroke();
-
-}
-
-document.onkeydown = move;
+)
 
 
-// console.log(canvas);
+
+
+
+
+// let canvas = document.getElementById('gameCanvas');
+// let context = canvas.getContext('2d');
+// let imgDrops;
+// let xPositionOfChar = 500;
+// let yPositionOfChar = 500;
+
+
+
+
+
+
+// //Part 1 - Creating Vickie and letting her move
+
+// function move(event) {
+
+// 	//Moving with right arrow key
+	
+// 	if (event.keyCode === 39) {
+// 		xPositionOfChar += 20;
+// 	}
+// 	//Moving with left arrow key
+	
+// 	if (event.keyCode === 37) {
+// 		xPositionOfChar -= 20;
+// 	}
+
+// 	//reseting the width of the canvas 
+
+// 	canvas.width = canvas.width;
+	
+// 	//setting the height, width and postition of Vickie
+	
+// 	context.rect(xPositionOfChar, yPositionOfChar, 30, 70);
+	
+// 	//allows the character show up on the canvas
+	
+// 	context.stroke();
+
+// }
+// //key event that will call the move function when key is pressed down
+// document.onkeydown = move;
+
+
+//Spawning veggies and meat from the sky
+
+
+
+
+
+
+
+})
