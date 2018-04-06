84e23c36b83b4187d3937119f4f7abbb9a531491
katcozadd <krosecozadd@gmail.com>
2018-04-06 10:57:40 -0600

    Started game logic and creating javascript elements to work with

--- js/game.js
+++ js/game.js
@@ -1,3 +1,30 @@
 let canvas = document.getElementById('gameCanvas');
 let context = canvas.getContext('2d');
-console.log(canvas);
+let imgDrops;
+let xPosition = 0;
+let yPosition = 0;
+let numberOfVegs = 50;
+let numberOfMeats = 25;
+var fallingItems = [];
+
+
+
+
+function move(event) {
+	if (event.keyCode === 39) {
+		xPosition += 20;
+	}
+	if (event.keyCode === 37) {
+		xPosition -= 20;
+	}
+
+	canvas.width = canvas.width;
+	context.rect(xPosition, yPosition, 20, 50);
+	context.stroke();
+
+}
+
+document.onkeydown = move;
+
+
+// console.log(canvas);
